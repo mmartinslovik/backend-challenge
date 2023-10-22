aa9080274b0f9d1a16c2c23136c7d013c2ea7215
Martin Slovík <xslovik@fi.muni.cz>
2023-10-22 15:30:22 +0200

    Add dtos

---
+++ src/main/java/com/example/taskmanager/api/TaskDto.java
@@ -0,0 +1,36 @@
+package com.example.taskmanager.api;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Schema(
+        name = "TaskDto",
+        description = "Data transfer object representing a Task."
+)
+public class TaskDto {
+
+    private Long id;
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime dueDate;
+
+    private LocalDateTime resolvedAt;
+
+    @NotBlank
+    @Size(max = 20)
+    private String title;
+
+    @Size(max = 50)
+    private String description;
+
+    private TaskPriority priority;
+
+    private TaskStatus status;
+}
---
+++ src/main/java/com/example/taskmanager/api/TaskStatus.java
@@ -0,0 +1,8 @@
+package com.example.taskmanager.api;
+
+public enum TaskStatus {
+
+    TODO,
+    IN_PROGRESS,
+    DONE
+}
